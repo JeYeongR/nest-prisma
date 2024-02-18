adf006b949a591d2593f77d4917e1daebc31873b
JeYeongR <wpdud2003@gmail.com>
2024-02-18 16:49:06 +0900

    Feat: 프리즈마 모듈 글로벌로 설정

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from '../prisma/prisma.module';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [PrismaModule],
 })
 export class UserModule {}
