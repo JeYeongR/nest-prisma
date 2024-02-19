38748aac80e2c3caeffa1f2ccdf77ddcb29d4813
JeYeongR <wpdud2003@gmail.com>
2024-02-20 00:10:54 +0900

    Test: UserController createUser() 단위 테스트 추가

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
-  let controller: UserController;
+  let userController: UserController;
+
+  const mockUserService = {
+    createUser: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+      ],
     }).compile();
 
-    controller = module.get<UserController>(UserController);
+    userController = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(userController).toBeDefined();
+  });
+
+  describe('createUser()', () => {
+    const createUserDto = {
+      email: 'test@test.com',
+      name: 'test',
+      password: 'test1234',
+    };
+
+    it('SUCCESS: UserService의 createUser()를 정상적으로 호출한다.', async () => {
+      // given
+      const spyCreateUserFn = jest.spyOn(mockUserService, 'createUser');
+
+      // when
+      const result = await userController.createUser(createUserDto);
+
+      // then
+      expect(result).toBeUndefined();
+      expect(spyCreateUserFn).toHaveBeenCalledTimes(1);
+      expect(spyCreateUserFn).toHaveBeenCalledWith(createUserDto);
+    });
   });
 });
