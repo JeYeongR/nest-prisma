cd5b9ee11477a805d5d4cff3fa9671f3d43bbf51
JeYeongR <wpdud2003@gmail.com>
2024-02-23 17:40:38 +0900

    Feat: 글로벌 가드 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenAuthGuard } from './common/guard/access-token-auth.guard';
 
 @Module({
   imports: [UserModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AccessTokenAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
