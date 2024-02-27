569f73b5726ff45a30a355657860688bd6f7cecb
JeYeongR <wpdud2003@gmail.com>
2024-02-27 22:33:49 +0900

    Chore: 투두 주석 삭제

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
-// TODO: Post 만들기
 @Module({
   controllers: [PostController],
   providers: [PostService],
