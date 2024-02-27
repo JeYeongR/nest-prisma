216c3693328d7d43b0ab9a1f7c719a4bfac06d77
JeYeongR <wpdud2003@gmail.com>
2024-02-27 20:05:00 +0900

    Test: PostService createPost() 단위 테스트 추가

---
+++ src/post/post.service.spec.ts
@@ -0,0 +1,124 @@
+import { HttpStatus, NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { Category } from './dto/post-category.enum';
+import { PostService } from './post.service';
+
+describe('PostService', () => {
+  let postService: PostService;
+
+  const mockPrisma = {
+    category: {
+      findFirst: jest.fn(),
+    },
+    post: {
+      create: jest.fn(),
+    },
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PostService,
+        {
+          provide: PrismaService,
+          useValue: mockPrisma,
+        },
+      ],
+    }).compile();
+
+    postService = module.get<PostService>(PostService);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(postService).toBeDefined();
+  });
+
+  describe('createPost()', () => {
+    const user = {
+      id: 1,
+    };
+    const createPostDto = {
+      title: 'test',
+      content: 'testtest',
+      published: null,
+      category: Category.place,
+    };
+    const mockCategory = {
+      id: 1,
+      name: Category.place,
+    };
+
+    it('SUCCESS: 성공적으로 포스트를 생성한다.', async () => {
+      // given
+      const spyPrismaCategoryFindFirstFn = jest.spyOn(
+        mockPrisma.category,
+        'findFirst',
+      );
+      spyPrismaCategoryFindFirstFn.mockResolvedValueOnce(mockCategory);
+      const spyPrismaPostCreateFn = jest.spyOn(mockPrisma.post, 'create');
+
+      // when
+      const result = await postService.createPost(user as User, createPostDto);
+
+      // then
+      expect(result).toBeUndefined();
+      expect(spyPrismaCategoryFindFirstFn).toHaveBeenCalledTimes(1);
+      expect(spyPrismaCategoryFindFirstFn).toHaveBeenCalledWith({
+        where: {
+          name: createPostDto.category,
+        },
+      });
+      expect(spyPrismaPostCreateFn).toHaveBeenCalledTimes(1);
+      expect(spyPrismaPostCreateFn).toHaveBeenCalledWith({
+        data: {
+          title: createPostDto.title,
+          content: createPostDto.content,
+          published: createPostDto.published,
+          user: {
+            connect: {
+              id: user.id,
+            },
+          },
+          category: {
+            connect: {
+              id: mockCategory.id,
+            },
+          },
+        },
+      });
+    });
+
+    it('FAILURE: 카테고리를 찾을 수 없으면 Not Found Exception을 반환한다.', async () => {
+      // given
+      const spyPrismaCategoryFindFirstFn = jest.spyOn(
+        mockPrisma.category,
+        'findFirst',
+      );
+      spyPrismaCategoryFindFirstFn.mockResolvedValueOnce(null);
+
+      // when
+      let hasThrown = false;
+      try {
+        await postService.createPost(user as User, createPostDto);
+
+        // Then
+      } catch (error) {
+        hasThrown = true;
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.getStatus()).toEqual(HttpStatus.NOT_FOUND);
+        expect(error.getResponse()).toEqual({
+          error: 'Not Found',
+          message: 'NOT_FOUND_CATEGORY',
+          statusCode: 404,
+        });
+      }
+      expect(hasThrown).toBeTruthy();
+    });
+  });
+});
